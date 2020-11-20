2a990b553bab6ceb3442f4071ee368ea39b62400
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-20 13:55:25 -0800

    purposefully fail constructor test.

---
+++ src/js/conversion.js
@@ -0,0 +1,7 @@
+export default class Conversion {
+  constructor (countryCode, rate, cashConverted) {
+    this.countryCode = countryCode;
+    this.rate = rate;
+    this.cashConverted = 5;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import ExchangeService from './js/exchange-service.js';
 
 function displayRates(response) {
   if (response.conversion_rates) {
-    // let countryCode = "AUS"
+    // let countryCode = "AUS";
     $("#show-rate").text(`1 United States Dollar equals ${response['conversion_rates']['AED']} in United Arab Emirates Dirham`);
   } else {
     $("#show-error").text(`There was an error: ${response}`);
