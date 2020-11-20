e217688699f53f5f9264d752340a84354b89f7c7
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-20 15:22:52 -0800

    Add some UI logic to incorporate conversion object

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './js/exchange-service.js';
+import Conversion from './js/conversion.js';
 
 function displayRates(response) {
   if (response.conversion_rates) {
-    // let countryCode = "AUS";
-    $("#show-rate").text(`1 United States Dollar equals ${response['conversion_rates']['AED']} in United Arab Emirates Dirham`);
+    $("#show-rate").text(`1 United States Dollar equals ${response.conversion_rates.AED} in United Arab Emirates Dirham`);
   } else {
     $("#show-error").text(`There was an error: ${response}`);
   }
@@ -15,12 +15,19 @@ function displayRates(response) {
 
 $(document).ready(function() {
   $("#money-button").click(function() {
-    // let amount = parseInt($("#amount").val());
     let promise = ExchangeService.getRates();
     promise.then(function(response) {
-      const body = JSON.parse(response); 
+      const body = JSON.parse(response);  
+      let conversion = new Conversion();
+      conversion.amountToConvert = parseInt($("#amount").val());
+      conversion.countryCode = $("#convert-to").val;
+      conversion.rate =`${body.conversion_rates.AED}`;
+      conversion.convertMonies();
+      console.log(conversion);
       displayRates(body);
+
     });
+
     // ExchangeService.getRates()
     //   .then(function(response){
     //     console.log(response);
